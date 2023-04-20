9649757b02a43ab009ad17ff26199d694da167cb
GleIL <intoger3@gmail.com>
2023-04-20 22:45:06 +0300

    Удаление затухяния с картинки

---
+++ script/changimg/ChangingImages.js
@@ -0,0 +1,15 @@
+let images = ["images/trv1.jpg","images/trv2.jpg","images/trv3.jpg","images/trv4.jpg","images/trv5.jpg","images/trv6.jpg"];
+
+let index = 0;
+
+function changeImage() {
+    const img = document.getElementById("image");
+    img.classList.add("fade-out"); // Добавляем класс с затуханием
+    setTimeout(() => {
+      index = (index + 1) % images.length;
+      img.src = images[index];
+      img.classList.remove("fade-out"); // Убираем класс с затуханием
+    }, 1000);
+  }
+
+setInterval(changeImage, 4000);
